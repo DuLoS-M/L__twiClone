8aca34b6fa3bc8c4a401f96602897acbd9ce1888
DuLoS-M <e_craud@hotmail.com>
2023-02-21 17:42:22 -0600

    ..

--- tweets/models.py
+++ tweets/models.py
@@ -5,3 +5,6 @@ class Tweet(models.Model):
 	content = models.TextField()
 	name = models.TextField(blank=True, null=True)
 	image = models.FileField(upload_to="images/", blank=True, null=True) 
+
+	def __str__(self):
+		return self.content
--- tweets/views.py
+++ tweets/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404, JsonResponse
 from .models import Tweet
+import random
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
@@ -20,3 +21,13 @@ def tweet_detail_view(request, tweet_id, *args, **kwargs):
         status = 404
     
     return JsonResponse(data=data, status=status)
+
+def tweet_list_view(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    tweets_list = [{"id": x.id, "content":x.content, "likes": random.randint(0, 100)} for x in qs]
+    data = {
+        "isUser": False,
+        "response": tweets_list
+    }
+
+    return JsonResponse(data=data)
